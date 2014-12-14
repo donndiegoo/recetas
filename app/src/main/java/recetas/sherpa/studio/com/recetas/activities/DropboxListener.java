bab9ccdf3db6a14b805896dc83deb725d7dce1ca
diego <diego.pinedo@sweetbits.es>
2014-12-14 14:03:52 +0100

    Dropbox api added

---
+++ app/src/main/java/recetas/sherpa/studio/com/recetas/activities/DropboxListener.java
@@ -0,0 +1,7 @@
+package recetas.sherpa.studio.com.recetas.activities;
+
+/**
+ * Created by diego on 13/12/14.
+ */
+public interface DropboxListener {
+}
---
+++ app/src/main/java/recetas/sherpa/studio/com/recetas/fragments/DropboxFragment.java
@@ -0,0 +1,7 @@
+package recetas.sherpa.studio.com.recetas.fragments;
+
+/**
+ * Created by diego on 13/12/14.
+ */
+public class DropboxFragment {
+}
